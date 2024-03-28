4f0b1d008b327c6f2d3c6731126be91902dce94f
Aditya Ranjan <aditya74810@gmail.com>
2024-03-29 00:50:10 +0530

    Changes on Hero Section

--- src/app/aditya/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div className="text-xl text-center text-black">This is Aditya Introduction Page</div>;
-};
-
-export default page;
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,38 +1,37 @@
-import Link from "next/link";
-import React from "react";
-import { Spotlight } from "./ui/Spotlight";
+import Link from "next/link"
+import { Spotlight } from "./ui/Spotlight"
 import { Button } from "./ui/moving-border";
 
 const HeroSection = () => {
   return (
-    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
-      <div className="p-4 relative z-10 w-full text-center">
+    <div
+    className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0"
+    >
         <Spotlight
-          className="-top-40 left-0 md:left-60 md:-top-20"
-          fill="lightyellow"
-        />
-        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400  ">
-          Master the art of music
-        </h1>
-        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
-          Dive into our comprehensive music cources and transform your musical
-          journey today. Whether you're a beginner or looking to refine your
-          skills, join us to unlock your true polential.{" "}
-        </p>
-        <div className="mt-4 ">
-          <Link href={"/cources"}>
-            {" "}
-            <Button
-              borderRadius="1.75rem"
-              className="bg-black dark:bg-slate-900 text-white dark:text-white border-neutral-200 dark:border-slate-800"
-            >
-              Explore cources
-            </Button>
-          </Link>
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+        <div className="p-4 relative z-10 w-full text-center" >
+            <h1
+            className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400"
+            >Master the art of music</h1>
+            <p
+            className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto"
+            >Dive into our comprehensive music courses and transform your musical journey today. Whether you're a beginner or looking to refine your skills, join us to unlock your true potential.</p>
+            <div className="mt-4">
+                <Link href={"/courses"}>
+                    <Button
+                    borderRadius="1.75rem"
+                    className="bg-white dark:bg-black text-black dark:text-white border-neutral-200 dark:border-slate-800"
+                    >
+                    Explore courses
+                    </Button>
+                </Link>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-};
+        
+        </div>
+  )
+}
 
-export default HeroSection;
+export default HeroSection
